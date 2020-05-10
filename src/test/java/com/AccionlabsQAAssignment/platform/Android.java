6b215b1540807b571dbe35ea41b071dd62d7b9dd
ravisingh171989 <Anand@Anand-MacBook-Pro.local>
2020-05-10 16:56:08 +0530

    added desired capability for android and iOS

---
+++ src/test/java/com/AccionlabsQAAssignment/platform/Android.java
@@ -0,0 +1,13 @@
+package com.AccionlabsQAAssignment.platform;
+
+/*All Desired capabilities*/
+public class Android {
+	public static final String platformName = "Android";
+	public static final String deviceName = "deviceName";
+	public static final String device = "Android";
+	public static final String automationName = "UiAutomator2";
+	public static final String URL = "http://127.0.0.1:4723/wd/hub";
+	public static final String browserNameChrome = "Chrome";
+	public static final String platformVersion = "";
+	public static final String appActivity = "org.chromium.chrome.browser.document.ChromeLauncherActivity";
+}
---
+++ src/test/java/com/AccionlabsQAAssignment/platform/IOS.java
@@ -0,0 +1,15 @@
+package com.AccionlabsQAAssignment.platform;
+
+/*iOS device related details*/
+public class IOS {
+	public static final String platformName = "iOS";
+	public static final String platformVersion = "11.3.1";
+	public static final String deviceName = "NotUsed";
+	public static final String device = "iOS";
+	public static final String udid = "1234####123";
+	public static final String URL = "http://127.0.0.1:4723/wd/hub";
+	public static final String browserNameSafari = "Safari";
+	public static final String appName = "**.ipa";
+	public static final String appDir = "";
+	public static final String automationName = "XCUITest";
+}
